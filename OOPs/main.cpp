e08e9e63d5a755ecedda3a10e940e6269b9b742a
Prince Kumar <singhprince799@outlook.com>
2023-08-04 22:32:35 +0530

    Oops practice

---
+++ OOPs/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Hero
+{
+    int health;
+    char level;
+};
+int main()
+{
+    Hero ramesh;
+    cout << "size of" << sizeof(ramesh);
+    return 0;
+}
